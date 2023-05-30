156ad2ad0ddbf5cef26c4d230942439677faeb21
Matt Maddux <matt@lumberstack.org>
2023-05-30 15:06:55 -0500

    Minor formatting changes

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -24,7 +24,7 @@ function yesNoPrompt {
 	echo $RESULT
 }
 
-echo "##### Configuring Generating Values #####"
+echo "##### Configuring Generating Values"
 
 read -p 'Domain Name: ' DOMAIN
 WWW_SUBDOMAIN=$(yesNoPrompt "Do you want to also add a 'www' subdomain for your domain?")
@@ -34,7 +34,7 @@ read -p 'Your Email: ' WP_USER_EMAIL
 PHP_VER=$(yesNoPrompt "Install PHP 8? (No for PHP 7.4)")
 
 
-echo "##### Generating Values #####"
+echo "##### Generating Values"
 WP_DIR="/var/www/wordpress"
 DB_ROOT_PASS=$(openssl rand -base64 36)
 DB_NAME="wordpress"
@@ -91,7 +91,7 @@ sudo a2dissite 000-default >> $LOG_FILE
 sudo a2ensite $DOMAIN >> $LOG_FILE
 
 
-echo "##### Installing & Configuring MySQL #####"
+echo "##### Installing & Configuring MySQL"
 sudo apt-get install mysql-server -y >> $LOG_FILE
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_ROOT_PASS'" >> $LOG_FILE																# Set root password
 mysql --password="$DB_ROOT_PASS" --user=root -e "DELETE FROM mysql.user WHERE User=''" 2>> $LOG_FILE >> $LOG_FILE																# Remove anonymous users
@@ -102,19 +102,17 @@ mysql --password="$DB_ROOT_PASS" --user=root -e "GRANT ALL ON wordpress.* TO '$D
 mysql -e "FLUSH PRIVILEGES" 2>> $LOG_FILE >> $LOG_FILE																															# Flush privileges
 
 
-
-echo "##### Installing PHP & Related Libraries #####"
 if [ $PHP_VER == true ]; then
-  echo "Installing PHP 8"
+  echo "##### Installing PHP 8 & Related Libraries"
   sudo apt-get install php php-mysql libapache2-mod-php php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y >> $LOG_FILE
 else
-  echo "Installing PHP 7"
+  echo "##### Installing PHP 7.4 & Related Libraries"
   sudo apt-get install php7.4 php7.4-mysql php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-soap php7.4-intl php7.4-zip libapache2-mod-php7.4 -y >> $LOG_FILE
 
 fi
 
 
-echo "##### Installing WP CLI #####"
+echo "##### Installing WP CLI"
 echo "Fetching"
 curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar >> $LOG_FILE
 chmod +x wp-cli.phar
@@ -124,21 +122,21 @@ echo -e "$WP_ALIAS" | sudo tee /usr/local/bin/wp >> $LOG_FILE
 sudo chmod +x /usr/local/bin/wp
 
 
-echo "##### Prepping Wordpress Directory #####"
+echo "##### Prepping Wordpress Directory"
 sudo rm -rf /var/www/html
 sudo mkdir $WP_DIR
 sudo chown www-data:www-data $WP_DIR
 sudo chmod 755 $WP_DIR
 
 
-echo "##### Installing Wordpress #####"wp co
+echo "##### Installing Wordpress"wp co
 cd $WP_DIR
 wp core download
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PASS
 wp core install --url="$DOMAIN" --title="$SITE_NAME" --admin_user="$WP_USER" --admin_password="$WP_USER_PASS" --admin_email="$WP_USER_EMAIL"
 
 
-echo "##### Stating Apache #####"
+echo "##### Stating Apache"
 sudo systemctl start apache2 >> $LOG_FILE
 
 echo -e "\033[31m ***** WARNING! *****\nYour passwords have been saved to $HOME/secrets.txt.\nMake sure you delete this file."
